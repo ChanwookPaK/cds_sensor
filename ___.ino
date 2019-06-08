08fdf94afbaa474659291649ec7f4e36c3992e2d
ChanwookPaK <51505903+ChanwookPaK@users.noreply.github.com>
2019-06-08 18:15:46 +0900

    Add files via upload

---
+++ ___.ino
@@ -0,0 +1,41 @@
+int valueleft;
+int valueright;
+int diff;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ int valueleft=analogRead(A0);
+ int valueright=analogRead(A3);
+
+ Serial.println(valueleft);
+ Serial.println(valueright);
+  
+  delay(500);
+
+  diff=valueleft-valueright
+  
+   if(diff<-40){
+  
+Serial.println(left);
+ 
+  }else if(diff<0){
+
+  Serial.println(center);
+ 
+  } else{
+
+  Serial.println(right);
+    
+  }
+  
+  
+}
